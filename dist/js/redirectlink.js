9c39da9ce5539e2e90233f25713474ef6cc0b385
ramzi-omari <ramziomari1995@gmail.com>
2021-03-06 14:39:26 +0100

    socials redirect links

---
+++ dist/js/redirectlink.js
@@ -0,0 +1,9 @@
+function tofb() {
+  window.open("https://www.facebook.com/ramzii.omari/", "_blank");
+}
+function toli() {
+  window.open("https://www.linkedin.com/in/ramzi-omari/", "_blank");
+}
+function togh() {
+  window.open("https://github.com/ramzi-omari", "_blank");
+}
