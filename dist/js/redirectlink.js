ec1f5e0b905d615fcf8b14101c631e4dc27aac34
ramzi-omari <ramziomari1995@gmail.com>
2021-03-07 23:09:08 +0100

    some projects add

--- dist/js/redirectlink.js
+++ dist/js/redirectlink.js
@@ -7,3 +7,25 @@ function toli() {
 function togh() {
   window.open("https://github.com/ramzi-omari", "_blank");
 }
+
+function memoriesfun() {
+  window.open("https://ram-mern-app-memories.netlify.app/", "_blank");
+}
+
+function NewsWebsite() {
+  window.open(" https://ram-news-static-website.netlify.app/", "_blank");
+}
+
+function hotelsimple() {
+  window.open("https://hotel-simpledesign-csshtml.netlify.app", "_blank");
+}
+
+function edgeLedgerWebsite() {
+  window.open("https://ram-edgeledger-simple.netlify.app", "_blank");
+}
+
+function recipeReact() {
+  window.open("https://ram-recipe-api-react.netlify.app/", "_blank");
+}
+
+//faut ajouter social medias clones & mern projects
